29b3c2641e4b3f422e4b410b7ff1f8a64f9665a5
jaxkodex <jorge12745@gmail.com>
2017-10-07 23:16:15 -0500

    fix paths

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-rm -rf dist
-mkdir -p dist/js
-mkdir -p dist/css
-mkdir -p dist/img
+rm -rf js
+rm -rf css
+rm -rf img
+mkdir -p js
+mkdir -p css
+mkdir -p img
 #browserify js/index.js > dist/js/index.js
 #rework -v webkit,moz < css/style.css > dist/css/style.css
-npm-css css/style.css > dist/css/style.css
-cp -R img/* dist/img/
-cp index.html dist/index.html
-cp google222e78e94f4b9974.html dist/google222e78e94f4b9974.html
+npm-css src/css/style.css > css/style.css
+cp -R src/img/* img/
+cp src/index.html index.html
+cp src/google222e78e94f4b9974.html google222e78e94f4b9974.html
